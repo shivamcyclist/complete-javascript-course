dd2f5ec1b21dbc6eba7bffc37cb28ca0b8d1207b
shivam <shivam12397@gmail.com>
2025-11-18 23:49:39 +0530

    Learning JS Day-1
    - Starting with the basics, variables, values, naming conventions and setup.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Shivam";
+console.log(firstName);
+
+let _name = "Shwetanki";
+console.log(_name);
